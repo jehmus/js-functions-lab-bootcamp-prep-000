cf75f42493277e0944060930da505c1263fc5501
Jaiman Chin <jaiman.chin@gmail.com>
2018-03-09 03:24:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+happyHolidayTo(name, holiday) {
+  return `Happy ${holiday}, ${name}!`
+}
